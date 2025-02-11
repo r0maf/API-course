4bced31d49b8f7f5fcc4082574054fba1430c574
r0maf <r9888582703@gmail.com>
2025-02-11 18:49:05 +0300

    Moving on

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from fastapi import FastAPI, Request
+from typing import Any, Optional
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "hafflo"}
+posts = {"posts_IDs": {}}
 
 
-@app.get("/posts/")
-def get_posts(data: Request):
-    return {"data": data.query_params}
+class Post(BaseModel):
+    addition_inf: Optional[str] = None
+    id: int
+    title: str
+    content: Any
+    rating: Optional[int] = None
+
+
+@app.get("/posts/{id}")
+def get_posts(id):
+    if int(id) in posts["posts_IDs"]:
+        return posts["posts_IDs"][int(id)]
+    else:
+        HTTPException(status_code=404, detail="we cannot find your post")
+
+
+@app.get("/posts")
+def get_all_posts():
+    return posts["posts_IDs"]
+
+
+@app.post("/posts")
+def create_posts(post: Post):
+    if post.id not in posts["posts_IDs"]:
+        posts["posts_IDs"][post.id] = post.model_dump()
+        return {
+            "message": f"post <<{post.title}>> has just been created",
+            "post": posts["posts_IDs"][post.id],
+        }
+    else:
+        raise HTTPException(409, detail="sorry, but the id already exists")
