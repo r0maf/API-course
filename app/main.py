9b1db92e2ee1977008b6597e45dc52bc746a8e56
r0maf <r9888582703@gmail.com>
2025-02-18 13:58:12 +0300

    Added a sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,60 +1,100 @@
-from typing import Any, Optional
-from fastapi import Body, FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Response, Depends
 from pydantic import BaseModel
+import psycopg2
+from sqlalchemy.orm import Session
+from psycopg2.extras import RealDictCursor
+import time
+from . import models
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-posts = {}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        database="fastapi",
+        user="postgres",
+        password="PostRomagres8876",
+        cursor_factory=RealDictCursor,
+    )
+    cursor = conn.cursor()
+    print("Database connection was successful")
+except Exception as error:
+    print("Database connection was failed")
+    print(f"Error: {error}")
+    time.sleep(2)
 
 
 class Post(BaseModel):
-    id: int
     title: str
-    content: Any
-    addition_inf: Optional[str] = None
-    rating: Optional[int] = None
+    content: str
+    published: bool = True
 
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "success"}
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    if id in posts:
-        return posts[id]
+    cursor.execute(f"""SELECT * FROM posts WHERE id = {id}""")
+    post = cursor.fetchone()
+    if post:
+        return post
     else:
         raise HTTPException(status_code=404, detail="we cannot find your post")
 
 
 @app.get("/posts")
 def get_all_posts():
+    cursor.execute("""SELECT * FROM posts ORDER BY id""")
+    posts = cursor.fetchall()
     return posts
 
 
 @app.post("/posts")
-def create_posts(post: Post):
-    if post.id not in posts:
-        posts[post.id] = post.model_dump(exclude="id")
-        return {
-            "message": f"post <<{post.title}>> has just been created",
-            "post_id": post.id,
-            "post": posts[post.id],
-        }
-    else:
-        raise HTTPException(409, detail="sorry, but the id already exists")
+def create_post(post: Post):
+    query = """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *"""
+    values = (post.title, post.content, post.published)
+    cursor.execute(query, values)
+    conn.commit()
+    return {"message": "The post was successfully created"}
 
 
 @app.delete("/posts/{id}", status_code=204)
 def delete_post(id: int):
-    if id in posts:
-        del posts[id]
-        return Response(status_code=204)
+    cursor.execute(f"""DELETE FROM posts WHERE id = {id} RETURNING True""")
+    posts = cursor.fetchone()
+    if posts:
+        conn.commit()
+        yield Response(status_code=204)
     else:
         raise HTTPException(status_code=404, detail="sorry, your element doesn't exist")
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, body: dict = Body(...)):
-    if id in posts:
-        for i in body:
-            posts[id][i] = body[i]
-        return {"updated_post": posts[id]}
+def update_post(id: int, body: Post):
+    query = f"""UPDATE posts SET title = %s, content = %s, published = %s, created_at = now() 
+                WHERE id = {id} RETURNING *"""
+    values = (body.title, body.content, body.published)
+    cursor.execute(query, values)
+    post = cursor.fetchone()
+    if post:
+        conn.commit()
+        return {
+            "information": {
+                "message": "The post was successfully updated",
+                "updated_post": post,
+            }
+        }
     else:
         raise HTTPException(status_code=404, detail="sorry, the id doesn't exist")
