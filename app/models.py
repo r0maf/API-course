1418bd42a4d3bf9bad2f99b38f2c8e4f5caf4bb4
r0maf <r9888582703@gmail.com>
2025-02-27 17:31:32 +0300

    I added a voting system with a new votes table

--- app/models.py
+++ app/models.py
@@ -21,6 +21,7 @@ class Post(Base):
 
     owner = relationship("User")
 
+
 class User(Base):
     __tablename__ = "users"
 
@@ -29,4 +30,15 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
     )
